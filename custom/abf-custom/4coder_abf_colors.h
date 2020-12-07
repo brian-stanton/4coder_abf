ccbce69d94b0076bc0dde51ced2b19ab09c90210
Brian Stanton <stanton.brian.c@gmail.com>
2020-12-07 12:22:54 -0500

    new file for managed id declarations for colors; this file is just included in 4coder_default_colors.h now

---
+++ custom/abf-custom/4coder_abf_colors.h
@@ -0,0 +1,19 @@
+#ifndef _4CODER_ABF_COLORS
+#define _4CODER_ABF_COLORS
+
+// NOTE(brian): Custom colors for syntax highlighting; added 2020/11/12
+CUSTOM_ID(colors, abf_primitive_highlight_type);
+CUSTOM_ID(colors, abf_primitive_highlight_function);
+CUSTOM_ID(colors, abf_primitive_highlight_macro);
+CUSTOM_ID(colors, abf_primitive_highlight_4coder_command);
+
+// NOTE(brian): More custom colors for syntax highlighting; added 2020/12/03
+CUSTOM_ID(colors, abf_color_keyword_subset);
+CUSTOM_ID(colors, abf_color_var_declaration);
+CUSTOM_ID(colors, abf_color_parens);
+
+// NOTE(brian): 2020/12/07
+CUSTOM_ID(colors, abf_color_active);
+CUSTOM_ID(colors, abf_color_background);
+
+#endif
